e08bd5dcc6920116cbde164a511e595befb4c77f
Mat <71132805+mat512@users.noreply.github.com>
2024-11-25 10:49:02 +0100

    feat: add task create

--- web/src/app/tasks/tasks.component.ts
+++ web/src/app/tasks/tasks.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { TaskCreateComponent } from "./task-create/task-create.component";
+
+interface Task {
+  name: string,
+  done: boolean
+}
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [TaskCreateComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
 export class TasksComponent {
-
+  public readonly tasks: Task[] = [
+    {
+      name: 'Test 1',
+      done: false
+    },
+    {
+      name: 'Test 2',
+      done: true
+    }
+  ]
 }
