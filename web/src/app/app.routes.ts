b394d7b53b49fbce09b42daab9ca959c11b5cf85
Mat <71132805+mat512@users.noreply.github.com>
2024-11-25 10:54:19 +0100

    refactor: folders

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./tasks/tasks.component').then((c) => c.TasksComponent)
+        loadComponent: () => import('./features/tasks/tasks.component').then((c) => c.TasksComponent)
     }
 ];
