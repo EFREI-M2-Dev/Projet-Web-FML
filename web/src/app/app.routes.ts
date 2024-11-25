93385e78e05c26bcba39f9992492cc4909eaded0
Mat <71132805+mat512@users.noreply.github.com>
2024-11-25 10:42:26 +0100

    feat: add tasks

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./tasks/tasks.component').then((c) => c.TasksComponent)
+    }
+];
