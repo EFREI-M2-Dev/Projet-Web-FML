b18a8b974cfcadff54f8c7d45e857cfb42997720
Suylo <spoownz@yahoo.com>
2024-11-25 15:26:14 +0100

    feat(task-item): date pipe format common modules

--- web/src/app/features/tasks/task-item/task-item.component.ts
+++ web/src/app/features/tasks/task-item/task-item.component.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from '../../../interfaces/Task';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-task-item',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.scss',
 })
