15bd6f67a0d72a4db34de1e7cba23f766efe9b44
Mat <71132805+mat512@users.noreply.github.com>
2024-11-26 15:50:25 +0100

    feat: add login message error

--- web/src/app/features/login/login.component.ts
+++ web/src/app/features/login/login.component.ts
@@ -13,17 +13,30 @@ import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 export class LoginComponent {
   email: string = '';
   password: string = '';
+  errorMessage: string | null = null; 
 
   constructor(private auth: Auth, private router: Router, private route: ActivatedRoute) {}
 
   login() {
+    this.errorMessage = null; 
+
     signInWithEmailAndPassword(this.auth, this.email, this.password)
       .then(() => {
         const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/tasks';
         this.router.navigate([returnUrl]);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Erreur de connexion', error);
+        this.errorMessage = this.getErrorMessage(error.code);
       });
+  }
+
+  private getErrorMessage(errorCode: string): string {
+    switch (errorCode) {
+      case 'auth/invalid-credential':
+        return "Identifiants invalide.";
+      default:
+        return "Une erreur inattendue est survenue. Veuillez réessayer.";
+    }
   }
 }
