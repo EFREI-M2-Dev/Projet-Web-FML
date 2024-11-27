81740c7b944e9c996c928285f92739a286af30c2
Mat <71132805+mat512@users.noreply.github.com>
2024-11-27 10:51:55 +0100

    feat: add thematic for edit

--- web/src/app/features/tasks/task-edit-modal/task-edit-modal.component.ts
+++ web/src/app/features/tasks/task-edit-modal/task-edit-modal.component.ts
@@ -1,7 +1,9 @@
-import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Task } from '../../../interfaces/Task';
 import { TasksFacade } from '../tasks.facade';
+import { ThematicService } from '../../../core/services/thematic.service';
+import { Thematic } from '../../../interfaces/Thematic';
 
 @Component({
   selector: 'app-task-edit-modal',
@@ -20,11 +22,19 @@ export class TaskEditModalComponent {
     thematic: '',
   };
 
-  private tasksFacade = inject(TasksFacade);
+  public thematics: Thematic[] = [];
+
+  constructor(
+    private tasksFacade: TasksFacade,
+    private thematicService: ThematicService,
+  ) {
+    this.thematicService.getThematics().subscribe((thematics) => {
+      this.thematics = thematics;
+    });
+  }
 
   public editTask() {
     this.tasksFacade.editTask(this.task);
-
     this.closeModal();
   }
 
