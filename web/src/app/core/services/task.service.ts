8a6f2dc93844a7c77df50fdc3968efcc436e8988
Mat <71132805+mat512@users.noreply.github.com>
2024-11-27 10:45:42 +0100

    feat: add thematic for creation

--- web/src/app/core/services/task.service.ts
+++ web/src/app/core/services/task.service.ts
@@ -30,7 +30,7 @@ export class TaskService {
       done: task.done,
       atDate: Timestamp.fromDate(task.atDate),
       userUID: task.userUID,
-      thematic: 'r2nuwSaJD0oE2KUE80gw',
+      thematic: task.thematic,
     };
 
     return addDoc(this.tasksCollection, taskDoc).then(() => {
--- web/src/app/features/tasks/task-create/task-create.component.ts
+++ web/src/app/features/tasks/task-create/task-create.component.ts
@@ -2,6 +2,8 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IconButtonComponent } from '../../../shared/icon-button/icon-button.component';
 import { NewTask } from '../../../interfaces/Task';
+import { ThematicService } from '../../../core/services/thematic.service';
+import { Thematic } from '../../../interfaces/Thematic';
 
 @Component({
   selector: 'app-task-create',
@@ -14,9 +16,16 @@ export class TaskCreateComponent {
 
   public isModalOpen = false;
   public newTask: NewTask = { title: '', description: '', atDate: new Date(), thematic: '' };
+  public thematics: Thematic[] = [];
+
+  constructor(private thematicService: ThematicService) {
+    this.thematicService.getThematics().subscribe((thematics) => {
+      this.thematics = thematics;
+    });
+  }
 
   public addTask() {
-    if (this.newTask.title.trim() && this.newTask.description.trim()) {
+    if (this.newTask.title.trim() && this.newTask.description.trim() && this.newTask.thematic) {
       this.newTask.atDate = new Date(this.newTask.atDate);
       this.taskAdded.emit(this.newTask);
 
